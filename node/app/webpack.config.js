2e22baafad503f2534c2e369fee2e87fcf326baa
chun_liang <wumegawa1020@gamil.com>
2018-11-17 15:37:00 +0900

    add babel to support old browser

--- node/app/webpack.config.js
+++ node/app/webpack.config.js
@@ -13,9 +13,33 @@ module.exports = {
         app: './src/index.js'
     },
     output: {
+        path: path.join(__dirname, "dist"),
         publicPath: "/js/",
         filename: '[name].js',
         library: ["com", "example"],
         libraryTarget: 'umd'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    "useBuiltIns": "usage",
+                                    "targets": "> 0.25%, not dead"
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    devtool: 'inline-source-map'
 };
